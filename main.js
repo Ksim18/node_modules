e13ff1cb7c9091fab9c1a38b7e06760a3e17ee90
Ksim18 <66336493+Ksim18@users.noreply.github.com>
2021-03-16 12:06:58 +0200

    Test
    zadanie2

---
+++ main.js
@@ -0,0 +1,8 @@
+const math = require("./node_modules/math/index")
+
+arr = [4, 54, 57, 64, 7, 4, 0, 55];
+a = [0, 1, 2, 3];
+
+console.log(math.min(arr));
+console.log(math.sum(a));
+console.log(math.abs(-228));
